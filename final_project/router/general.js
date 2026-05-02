5f6a7d7ab0f895b971d2a967da677b3be15af5a3
Tsoi Wai Mun <waimun.t@nanolumi.com>
2026-05-02 16:09:01 +0800

    completed Task 3

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -24,8 +24,13 @@ public_users.get('/isbn/:isbn',function (req, res) {
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const author = req.params.author;
+  const filteredBooks = Object.values(books).filter(book => book.author === author);
+  if (filteredBooks.length > 0) {
+    return res.status(200).send(JSON.stringify(filteredBooks,null,2));
+  } else {
+    return res.status(404).json({message: "No books found for the given author"});
+  }
 });
 
 // Get all books based on title

c0a9c0a413f7943a604906358aba9fba81a092be
Tsoi Wai Mun <waimun.t@nanolumi.com>
2026-05-02 16:06:32 +0800

    completed Task 2

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -18,8 +18,8 @@ public_users.get('/',function (req, res) {
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  return res.status(200).send(JSON.stringify(books[isbn],null,2));
  });
   
 // Get book details based on author

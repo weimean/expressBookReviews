4e4a15eb747371e8b8b9e3ea9c6291b8c5964e53
Tsoi Wai Mun <waimun.t@nanolumi.com>
2026-05-02 16:54:19 +0800

    Task 11: Get book by ISBN using async-await

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -33,10 +33,20 @@ public_users.get('/',async function (req, res) {
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-  const isbn = req.params.isbn;
-  return res.status(200).send(JSON.stringify(books[isbn],null,2));
- });
+public_users.get('/isbn/:isbn',async function (req, res) {
+  try {
+    const isbn = req.params.isbn;
+    const book = await Promise.resolve(books[isbn]);
+
+    if (book) {
+      return res.status(200).send(JSON.stringify(book,null,2));
+    } else {
+      return res.status(404).json({message: "Book not found"});
+    }
+  } catch (error) {
+    return res.status(500).json({message: "Error fetching book details"});
+  }
+});
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {

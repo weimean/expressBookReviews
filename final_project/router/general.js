e4463c8d8a7de3be29f8ff2a6a1d9133d849b1ce
Tsoi Wai Mun <waimun.t@nanolumi.com>
2026-05-02 16:15:22 +0800

    completed Task 6

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -6,8 +6,19 @@ const public_users = express.Router();
 
 
 public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    return res.status(400).json({message: "Username and password are required"});
+  }
+
+  const existingUser = users.find(user => user.username === username);
+  if (existingUser) {
+    return res.status(400).json({message: "Username already exists"});
+  }
+  
+  users.push({ username, password });
+  return res.status(200).json({message: "User successfully registered. Now you can login"});
 });
 
 // Get the book list available in the shop
